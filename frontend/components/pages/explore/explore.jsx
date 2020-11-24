f6b425365a6c22685641c4deed6140062e1ba4f3
Steven Suazo <stevensuazo@gmail.com>
2020-11-24 00:11:45 -0500

    added style to index page

--- frontend/components/pages/explore/explore.jsx
+++ frontend/components/pages/explore/explore.jsx
@@ -11,17 +11,57 @@ class photoIndex extends React.Component {
   }
 
   render() {
-    
-    const allPhotos = this.props.photos.map(photo => {
-      return <img src={photo.pictureUrl} key={photo.id} />
-    })
+    // const { photos } = this.props;
 
     return (
-      <div>
-        {allPhotos}
+      <div className="main-photo-index-div">
+
+        <div className="photos-container">
+          <ul className="photos-ul">
+            {
+              this.props.photos.map(photo =>
+                <div className="photo-box"
+                  key={photo.id}>
+
+                  {/* <div className="photo-index-user-info">
+                    <Link
+                      className="photo-index-username"
+                      to={`/users/${photo.user.id}`}>
+                      {photo.user.username}
+                    </Link>
+                    <h2 className="featured">Featured</h2>
+                  </div> */}
+
+                  <ul className="inner-box">
+                    <img
+                      className="index-photo"
+                      src={photo.pictureUrl} />
+                  </ul>
+
+                  <div className="photo-title-container">
+                    <h2 className="photo-title">{photo.title}</h2>
+                  </div>
+                </div>
+              )}
+          </ul>
+        </div>
       </div>
     )
+
   }
 }
 
 export default photoIndex; 
+
+
+
+
+// const allPhotos = this.props.photos.map(photo => {
+//   return <img src={photo.pictureUrl} key={photo.id} />
+// })
+
+// return (
+//   <div>
+//     {allPhotos}
+//   </div>
+// )
